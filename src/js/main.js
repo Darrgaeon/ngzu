dd6a836252d04e6f8ed8bb1c9e1cf7035d60fc10
Darrgaeon <Darrgaeon@yandex.ru>
2019-02-04 18:23:00 +0500

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// prevent babel from losing this
+// this should be window object
+
+function init() {
+    this.$ = require("jquery/dist/jquery");
+    this.jQuery = this.$;
+
+    require("svg4everybody/dist/svg4everybody")();
+    // require("retinajs/dist/retina");
+    require("uikit/dist/js/uikit");
+    require("uikit/dist/js/uikit.min");
+
+    $(document).ready(function () {
+
+    });
+}
+
+init.bind(window)();
