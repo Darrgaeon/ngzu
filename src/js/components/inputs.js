f7649ebb11f77966b9624f3c33314e75c150f4ca
Darrgaeon <Darrgaeon@yandex.ru>
2019-02-21 13:39:06 +0500

    Added js for inputs on page "Catalog-product"

--- src/js/components/inputs.js
+++ src/js/components/inputs.js
@@ -3,30 +3,46 @@ module.exports = function () {
     const $productVariantFirstArrayItems = $(".product-variant--first .product-variant-item");
     const $productVariantThirdArrayItems = $(".product-variant--third .product-variant-item");
 
-    const $customRadioBlock = $(".custom-radio-block");
+    const $catalogResultCompare = $(".catalog-result-compare");
+
+    const customRadioBlock = (".custom-radio-block");
+    const customCheckboxBlock = (".custom-checkbox-block");
 
     const checkboxBeforeShow = "checkbox-before-show";
 
     const ArrayTypeChoices = ["type-choice-1", "type-choice-2", "type-choice-3", "type-choice-4"];
     const ArrayControlChoices = ["control-choice-1", "control-choice-2"];
 
+    // Items in block Product-variant
     $productVariant.on("click", "label input", function () {
         const id = $(this).attr("id");
 
         $.each(ArrayTypeChoices, function (index, value) {
             if (id === value) {
-                $productVariantFirstArrayItems.children($customRadioBlock).removeClass(checkboxBeforeShow);
+                $productVariantFirstArrayItems.children(customRadioBlock).removeClass(checkboxBeforeShow);
             }
         });
 
         $.each(ArrayControlChoices, function (index, value) {
             if (id === value) {
-                $productVariantThirdArrayItems.children($customRadioBlock).removeClass(checkboxBeforeShow);
+                $productVariantThirdArrayItems.children(customRadioBlock).removeClass(checkboxBeforeShow);
             }
         });
 
-        $(this).siblings(".custom-radio-block").toggleClass(checkboxBeforeShow);
-        $(this).siblings(".custom-checkbox-block").toggleClass(checkboxBeforeShow);
+        $(this).siblings(customRadioBlock).toggleClass(checkboxBeforeShow);
+        $(this).siblings(customCheckboxBlock).toggleClass(checkboxBeforeShow);
+    });
+
+    //Сравнение
+    $catalogResultCompare.on("click", "input", function () {
+        $(this).siblings(customCheckboxBlock).toggleClass(checkboxBeforeShow);
+
+        if ($(this).siblings(customCheckboxBlock).hasClass(checkboxBeforeShow)) {
+            $(this).siblings("span").text("Убрать из сравнения")
+        } else {
+            $(this).siblings("span").text("Добавить в сравнение")
+        }
+
     });
 
 };
